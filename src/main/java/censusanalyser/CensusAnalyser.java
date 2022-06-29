1a405b938c4ff9ee6acd9c2155ee7c0fb829b5d9
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-29 14:16:22 +0530

    added test case to throw censusanalyser exception if csv file path is wrong

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -12,22 +12,24 @@ import java.util.Iterator;
 
 public class CensusAnalyser {
 
-    public int loadIndiaCensusData(String csvFilePath) throws IOException {
+    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaCensusCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+            int namOfEateries = 0;
+            while (censusCSVIterator.hasNext()) {
+                namOfEateries++;
+                IndiaCensusCSV censusData = censusCSVIterator.next();
+            }
+            return namOfEateries;
 
-        Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-        CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-        csvToBeanBuilder.withType(IndiaCensusCSV.class);
-        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-        CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-        Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
-        ;
-        int namOfEateries = 0;
-        while (censusCSVIterator.hasNext()) {
-            namOfEateries++;
-            IndiaCensusCSV censusData = censusCSVIterator.next();
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
-        return namOfEateries;
-
     }
-
 }
